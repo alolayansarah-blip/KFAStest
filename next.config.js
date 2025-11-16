2bedf82ab34a69cf1bffd6ed372e00128b570644
t000085-stack <t000085@coded.edu.kw>
2025-11-16 23:01:24 +0300

    Configure Next.js to ignore ESLint during builds for Heroku

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: false,
+  },
 };
 
 module.exports = nextConfig;
